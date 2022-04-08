9efccb49dd9b54fc87203f527c6913795691450e
SpeedZDev <98675043+SpeedZDev@users.noreply.github.com>
2022-04-08 19:13:30 -0400

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,43 @@
+function Colorbuttonclick(){
+   document.getElementById("script").style.color = "blue"
+}
+
+function Colorbuttonclick2(){
+   document.getElementById("script").style.color = "red"
+}
+
+function Colorbuttonclick3(){
+   document.getElementById("script").style.color = "green"
+}
+
+function Colorbuttonclick4(){
+   document.getElementById("script").style.color = "yellow"
+}
+
+function Colorbuttonclick5(){
+   document.getElementById("script").style.color = "Black" 
+}
+
+function Backroundbuttonclick(){
+   document.getElementById("Backround").style.backgroundColor = "black" 
+}
+
+function Backroundbuttonclick2(){
+   document.getElementById("Backround").style.backgroundColor = "white"  
+}
+
+function Backroundbuttonclick3(){
+   document.getElementById("Backround").style.backgroundColor = "gold"   
+}
+
+function Colorbuttonclick6(){
+   document.getElementById("Backround").style.backgroundColor = "pink"   
+}
+
+function Backroundbuttonclick4(){
+   document.getElementById("Backround").style.backgroundColor = "purple"  
+}
+
+function Backroundbuttonclick5(){
+   document.getElementById("Backround").style.backgroundColor = "white"  
+}
